ac5ce026de495ab9a2a42cb677ec6c760388d5cd
Benoit Garcia <benoit@terra-art.net>
2019-06-04 09:35:21 +0200

    update(module): Upgrade to terraform v0.12.0 and Scaleway provider v1.10.0.

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "scaleway_server" "server" {
-  enable_ipv6 = "${var.enable_ipv6}"
-  image       = "${var.image_name}"
-  name        = "${var.server_name}"
-  state       = "${var.server_state}"
-  type        = "${var.server_type}"
+  enable_ipv6 = var.enable_ipv6
+  image       = var.image_name
+  name        = var.server_name
+  state       = var.server_state
+  type        = var.server_type
 }
 
 resource "scaleway_ip" "public_ipv4" {
-  count   = "${var.enable_ipv4}"
-  server  = "${scaleway_server.server.id}"
+  count  = var.enable_ipv4
+  server = scaleway_server.server.id
 }
+
--- variables.tf
+++ variables.tf
@@ -9,27 +9,28 @@ variable "enable_ipv6" {
 }
 
 variable "image_name" {
-  type        = "string"
+  type        = string
   description = "Image ID. Defaut to Ubuntu Xenial arm"
   default     = "f71f2ad9-7810-405b-9181-2e8d5e1feb18"
-#  default     = "b5a754d1-8262-47d2-acb2-22739295bb68" # Ubuntu Xenial x86_64
+  #  default     = "b5a754d1-8262-47d2-acb2-22739295bb68" # Ubuntu Xenial x86_64
 }
 
 variable "server_name" {
-  type        = "string"
+  type        = string
   description = "Server name. Default to 'server'"
   default     = "server"
 }
 
 variable "server_state" {
-  type        = "string"
+  type        = string
   description = "Server state; either stopped or running Default to 'stopped"
   default     = "stopped"
 }
 
 variable "server_type" {
-  type        = "string"
+  type        = string
   description = "Server type. Default to 'C1'"
   default     = "C1"
   #  default     = "DEV1-S"
 }
+
