a80809ee3c8957d5d8875f6b6c98ce6a2f1c0cca
Benoit Garcia <benoit@terra-art.net>
2020-02-24 14:52:37 +0100

    update(module): Update to Scaleway provider 1.13.0:
    - Fix grammar.

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-resource "scaleway_instance_server" "server" {
+resource "scaleway_instance_server" "instance_with_ip" {
+  count       = var.enable_ipv4 == true ? 1 : 0
+  enable_ipv6 = var.enable_ipv6
+  image       = var.image_name
+  ip_id       = var.public_ipv4
+  name        = var.server_name
+  state       = var.server_state
+  type        = var.server_type
+}
+
+resource "scaleway_instance_server" "server_without_ip" {
+  count       = var.enable_ipv4 == true ? 0 : 1
   enable_ipv6 = var.enable_ipv6
   image       = var.image_name
   name        = var.server_name
@@ -8,5 +19,4 @@ resource "scaleway_instance_server" "server" {
 
 resource "scaleway_instance_ip" "public_ipv4" {
   count     = var.enable_ipv4 == true ? 1 : 0
-  server_id = scaleway_server.server.id
 }
