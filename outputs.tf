4950ed702281e3da9fb866d1034217e7237711e5
Benoit Garcia <benoit.garcia@protonmail.com>
2023-11-28 07:04:16 +0100

    fix: Fix outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "ip4" {
   description = "IPv4 address of the intance."
-  value       = var.enable_public_ipv4 ? scaleway_instance_server.this.public_ip : scaleway_instance_server.this.private_ip
+  value       = var.enable_public_ipv4 ? (var.routed_ip_enabled ? scaleway_instance_ip.public_ipv4[0].address : scaleway_instance_server.this.public_ip) : scaleway_instance_server.this.private_ip
 }
 
 output "ip6" {
   description = "IPv6 address of the instance."
-  value       = scaleway_instance_server.this.ipv6_address
+  value       = var.enable_ipv6 && var.routed_ip_enabled ? one([for item in scaleway_instance_server.this.public_ips[*].address : item if can(regex(":", item))]) : scaleway_instance_server.this.ipv6_address
 }
 
 output "name" {
