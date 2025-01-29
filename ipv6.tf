3fa0c59058dfb3625950da61ac5d309f7ed2c0de
Benoit Garcia <benoit.garcia@protonmail.com>
2025-01-29 22:10:45 +0100

    chore(instance)!: Phased out nat IPs and force use of routed IPs

--- ipv6.tf
+++ ipv6.tf
@@ -4,7 +4,7 @@ moved {
 }
 
 resource "scaleway_instance_ip" "ipv6" {
-  count = var.enable_ipv6 && var.routed_ip_enabled ? 1 : 0
+  count = var.enable_ipv6 ? 1 : 0
 
   project_id = var.project_id
   type       = "routed_ipv6"
@@ -12,7 +12,7 @@ resource "scaleway_instance_ip" "ipv6" {
 }
 
 resource "scaleway_instance_ip_reverse_dns" "ipv6" {
-  count = var.enable_ipv6 && var.routed_ip_enabled && (var.domainname != null) ? 1 : 0
+  count = var.enable_ipv6 && (var.domainname != null) ? 1 : 0
 
   ip_id   = scaleway_instance_ip.ipv6[count.index].id
   reverse = local.effective_fqdn
@@ -22,7 +22,7 @@ resource "scaleway_instance_ip_reverse_dns" "ipv6" {
 resource "scaleway_domain_record" "ipv6" {
   count = var.domainname != null && var.enable_ipv6 && var.state != "stopped" ? 1 : 0
 
-  data     = var.routed_ip_enabled ? one([for item in scaleway_instance_server.this.public_ips[*].address : item if can(regex(":", item))]) : scaleway_instance_server.this.ipv6_address
+  data     = one([for item in scaleway_instance_server.this.public_ips[*].address : item if can(regex(":", item))])
   dns_zone = var.domainname
   name     = local.effective_hostname
   type     = "AAAA"
