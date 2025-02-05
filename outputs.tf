b0572c4abd9b8aeb406c44928f5a5ca9388dd467
Benoit Garcia <72207524+benoit-garcia@users.noreply.github.com>
2025-02-05 19:55:20 +0100

    chore(ip): Stop using deprecated attributes (#64)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "ip4" {
   description = "IPv4 address of the intance."
-  value       = var.enable_public_ipv4 ? scaleway_instance_ip.public_ipv4[0].address : scaleway_instance_server.this.private_ip
+  value       = var.enable_public_ipv4 ? one(scaleway_instance_ip.public_ipv4[*].address) : one(data.scaleway_ipam_ip.private_ipv4[*].address)
 }
 
 output "ip6" {
   description = "IPv6 address of the instance."
-  value       = var.enable_ipv6 ? one([for item in scaleway_instance_server.this.public_ips[*].address : item if can(regex(":", item))]) : scaleway_instance_server.this.ipv6_address
+  value       = var.enable_ipv6 ? one([for item in scaleway_instance_server.this.public_ips[*].address : item if can(regex(":", item))]) : null
 }
 
 output "name" {
